7ff2693e9a516a33110b2d5e154787ed60c8ecae
paul-hartwich <paulnoahhartwich@gmail.com>
2025-01-29 00:15:45 +0100

    Commit

--- can_of_proxy/proxy_data_manager.py
+++ can_of_proxy/proxy_data_manager.py
@@ -5,13 +5,13 @@ from json import JSONDecodeError
 
 
 def _validate_protocol(protocol: str) -> str:
-    if protocol not in ("http", "https", "socks4", "socks5"):
+    if protocol not in ("http", "https", "socks4", "socks5", None):
         raise ValueError("Invalid protocol used to create ProxyDataManager")
     return protocol
 
 
 class ProxyDataManager:
-    def __init__(self, json_file: Path = True, preferred_protocol: str = "http", preferred_country: str = None,
+    def __init__(self, json_file: Path = None, preferred_protocol: str = None, preferred_country: str = None,
                  preferred_anonymity: str = None, allowed_fails_in_row: int = 3, fails_without_check: int = 2,
                  dont_store_data: bool = False, percent_failed_to_remove: float = 0.5):
         """
@@ -23,7 +23,8 @@ class ProxyDataManager:
         :param allowed_fails_in_row: The number of times a proxy can fail in a row before it gets removed.
         :param fails_without_check: The number of times a proxy can fail without being checked before it gets removed.
         :param dont_store_data: If the data should be stored in the JSON file or not. NOT RECOMMENDED!
-        :param percent_failed_to_remove: The percentage of failed requests that a proxy can have before it gets removed.
+        :param percent_failed_to_remove: Example: 0.5 fails: a success: b, if a/(a+b) > percent_failed_to_remove, then the proxy will be removed.
+        The lower the value, the more likely the proxy will be removed because of a high chance to fail.
         """
         self.dont_store_data = dont_store_data
 
@@ -83,7 +84,7 @@ class ProxyDataManager:
         If no proxy left, you should try other sources for proxy.
         """
         if self.last_proxy_index is not None:
-            self.rm_proxy(self.last_proxy_index)
+            self._rm_proxy(self.last_proxy_index)
 
     def feedback_proxy(self, success: bool):
         if self.last_proxy_index is None or self.last_proxy_index >= len(self.proxies):
@@ -101,19 +102,21 @@ class ProxyDataManager:
             # Check if the consecutive failure count exceeds the allowed limit
             if self.proxies[self.last_proxy_index]["times_failed_in_row"] > self.allowed_fails_in_row:
                 # Remove the proxy if it has failed too many times consecutively
-                self.rm_proxy(self.last_proxy_index)
+                self._rm_proxy(self.last_proxy_index)
             # Check if the total failure count exceeds the allowed limit without a check
             elif self.proxies[self.last_proxy_index][
                 "times_failed"] > self.fails_without_check:
-                total = self.proxies[self.last_proxy_index]["times_failed"] + self.proxies[self.last_proxy_index][
-                    "times_succeed"]
+                fail_success_ratio = self.proxies[self.last_proxy_index]["times_failed"] / (
+                        self.proxies[self.last_proxy_index]["times_failed"] +
+                        self.proxies[self.last_proxy_index][
+                            "times_succeed"])  # Calculate the failure rate: percentage of failures on average
                 # Remove the proxy if the failure rate is more than 50%
-                if self.proxies[self.last_proxy_index]["times_failed"] / total > self.percent_failed_to_remove:
-                    self.rm_proxy(self.last_proxy_index)
+                if fail_success_ratio > self.percent_failed_to_remove:  # the likeliness of the proxy being bad is higher than max allowed
+                    self._rm_proxy(self.last_proxy_index)
         # Update the JSON file with the current state of proxies
         self._write_data()
 
-    def add_proxy(self, proxy: IP, country: str | None = None, anonymity: str | None = None):
+    def add_proxy(self, proxy: IP, country: str | None = None, anonymity: str | int | None = None):
         """
         Add a proxy to the list with the right format.
 
@@ -132,7 +135,7 @@ class ProxyDataManager:
         }
         self.proxies.append(proxy_data)
 
-    def rm_proxy(self, index: int):
+    def _rm_proxy(self, index: int):
         try:
             self.proxies.pop(index)
         except IndexError:
@@ -177,7 +180,7 @@ class ProxyDataManager:
 
 
 if __name__ == '__main__':
-    manager = ProxyDataManager(dont_store_data=True)
+    manager = ProxyDataManager()
     manager.add_proxy(IP(url="http://123.123.123.123:1020"))
     manager.add_proxy(IP(url="http://123.123.123.123:2020"))
     manager.add_proxy(IP(url="http://123.123.123.123:3020"))
