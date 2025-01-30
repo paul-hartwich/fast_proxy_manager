3751732f43124f529c528d75d8241e012d39b02b
paul-hartwich <paulnoahhartwich@gmail.com>
2025-01-30 16:51:56 +0100

    Commit

--- can_of_proxy/utils/fetch_proxies.py
+++ can_of_proxy/utils/fetch_proxies.py
@@ -1,24 +1,48 @@
 import aiohttp
 from yarl import URL
+import asyncio
 
 
 async def http_github() -> list[URL]:
-    async def get():
-        url = "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt"
-        protocol = "http"
-        # every like a new http proxy with the format: ip:port
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                urls = []
-                for line in (await response.text()).split("\n"):
-                    if line:
-                        urls.append(URL(f"{protocol}://{line}"))
-                return urls
+    """
+    :return: List of http proxies from TheSpeedX GitHub repository.
+    """
+    url = "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt"
+    protocol = "http"
+    # every line is a new http proxy with the format: ip:port
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            urls = []
+            for line in (await response.text()).split("\n"):
+                if line:
+                    urls.append(URL(f"{protocol}://{line}"))
+            return urls
 
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(http_github())
+
+async def github(country, anonymity) -> list[URL]:
+    """
+    When just all proxies are fetched, they can still be filtered by country and anonymity.
+    :return: List of http proxies from proxifly GitHub repository.
+    """
+    url = "https://github.com/proxifly/free-proxy-list/tree/main"
+    protocol = "http"
+    # every line is a new http proxy with the format: ip:port
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            urls = []
+            for line in (await response.text()).split("\n"):
+                if line:
+                    urls.append(URL(f"{protocol}://{line}"))
+            return urls
+
 
 if __name__ == '__main__':
-    import asyncio
     from pprint import pprint
-    pprint(http_github())
+
+
+    async def main():
+        proxies = await http_github()
+        pprint(proxies)
+
+
+    asyncio.run(main())
