568d0a8c7dc32faf93e6c11884f3fe9782ace629
paul-hartwich <paulnoahhartwich@gmail.com>
2025-02-01 10:36:44 +0100

    Commit

--- can_of_proxy/test_proxies.py
+++ can_of_proxy/test_proxies.py
@@ -5,14 +5,7 @@ from icecream import ic
 from utils import URL
 
 
-class ProxyConfig:
-    timeout = 15
-    allow_redirects = True
-    headers = {"User-Agent": "Mozilla/5.0", "Accept-Encoding": "identity"}
-
-
-async def _is_proxy_valid(proxy: Dict, session: aiohttp.ClientSession,
-                          supported_protocols: Tuple[str] = ('http', 'https')) -> Union[str, None]:
+async def _is_proxy_valid(proxy: Dict, supported_protocols: Tuple[str] = ('http', 'https')) -> Union[str, None]:
     """Proxy dictionary must contain 'url' or 'proxy' key or 'ip', 'port', 'protocol' keys"""
     try:
         protocol = proxy['protocol']
@@ -24,47 +17,42 @@ async def _is_proxy_valid(proxy: Dict, session: aiohttp.ClientSession,
         url = URL(url)
         protocol = url.protocol
 
-    try:
-        if protocol not in supported_protocols:
-            raise ValueError(f"Unsupported protocol: {protocol}")
+    use_ssl: bool = False
+    if proxy.get('ssl') is True:
+        use_ssl = True
 
-        async with session.get("https://httpbin.org/ip", proxy=str(url), allow_redirects=ProxyConfig.allow_redirects,
-                               timeout=ProxyConfig.timeout, headers=ProxyConfig.headers) as response:
-            if response.status == 200:
-                ic(f"Valid: {url}")
-                return str(url)
-            return None
-    except aiohttp.ClientHttpProxyError as e:
-        print(f"Proxy error: {e}")
+    if protocol not in supported_protocols:
         return None
-    except aiohttp.ClientConnectionError as e:
-        print(f"Connection closed: {e}")
-        return None
-    except asyncio.TimeoutError:
-        print(f"Timeout error: {url}")
-        return None
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-        return None
-    except ConnectionResetError as e:
-        print(f"Connection reset error: {e}")
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://httpbin.org/ip", proxy=url,
+                                   allow_redirects=True,
+                                   timeout=15,
+                                   ssl=use_ssl) as response:
+                if response.status == 200:
+                    ic(f"Valid: {url}")
+                    return url
+                return None
+    except (asyncio.TimeoutError, aiohttp.ClientError, AssertionError, OSError, RuntimeError,
+            ConnectionResetError, aiohttp.ClientConnectionError, aiohttp.ClientHttpProxyError):
+        print("Proxy is invalid")
         return None
 
 
 async def get_valid_proxies(proxies: List[Dict]) -> Tuple[bool, Union[List[Dict], None]]:
     """Dict must contain 'url' key or preferably 'ip' and 'port' keys."""
     valid_proxies = []
-    async with aiohttp.ClientSession() as session:
-        for proxy in proxies:
-            valid, formatted_proxy = await _is_proxy_valid(proxy, session)
-            if valid:
-                valid_proxies.append(formatted_proxy)
-                ic(f"Valid: {formatted_proxy}")
-        return True, valid_proxies
+    for proxy in proxies:
+        valid = await _is_proxy_valid(proxy)
+        if valid is not None:
+            valid_proxies.append(valid)
+            ic(f"Valid: {valid}")
+    return valid_proxies
 
 
 if __name__ == '__main__':
-    from get import github_proxifly, custom_json_proxy_list
+    from get import custom_json_proxy_list
 
 
     async def main():
